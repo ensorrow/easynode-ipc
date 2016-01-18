6debee671f65aa0e6cb417022a7b694b739fd7d4
胡家宝 <jiabao_hu@corp.netease.com>
2016-01-18 23:49:12 +0800

    node-nos be abolished, change to neos

--- src/netease/icp/backend/services/LoginService.js
+++ src/netease/icp/backend/services/LoginService.js
@@ -5,7 +5,6 @@ var md5 =  require('md5');
 var fs = require('co-fs');
 var f =  require('fs');
 var bfs = require('babel-fs');
-var Nos = require('node-nos');
 var archiver = require('archiver');
 var _ = require('lodash');
 var StoreService = using('netease.icp.backend.services.StoreService');
