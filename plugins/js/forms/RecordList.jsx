5ed852f51ea0c758d2ad939aacbc0f7496d2a138
胡家宝 <jiabao_hu@corp.netease.com>
2016-01-28 14:49:02 +0800

    l.customizeLang and merge handleDetial & handleModify request

--- plugins/js/forms/RecordList.jsx
+++ plugins/js/forms/RecordList.jsx
@@ -71,6 +71,17 @@ let Operation = React.createClass({
 
     },
     handleModify: function(){
+        this.requestRecord(
+            function(){
+                location.href = "#/returntobase";
+            },
+            function(err){
+                console.log("getRecord err")
+                console.log(err);
+            }
+        );
+    },
+    requestRecord: function(succ,err){
         var tenantId = __globals__.user == undefined ? '111111' : __globals__.user.tenantId;
 
         reqwest({
@@ -131,88 +142,23 @@ let Operation = React.createClass({
                 __globals__.material.protocolurl2 = record.protocolurl2;
                 __globals__.material.securityurl1 = record.securityurl1;
                 __globals__.material.securityurl2 = record.securityurl2;
-
-                location.href = "#/returntobase";
+                succ();
             },
-            error: function(err){
-                //TODO
+            error: function(e){
+                err(e);
             }
         });
-
-        return true;
     },
     handleDetail: function(){
-        var tenantId = __globals__.user == undefined ? '111111' : __globals__.user.tenantId;
-
-        reqwest({
-            url: '/getrecord',
-            method: 'post',
-            data: JSON.stringify({id:this.props.record.id}),
-            type:'json',
-            contentType: 'application/json',
-            success: function(resp){
-                console.log(resp.ret);
-                var record  = resp.ret.record;
-                var company = resp.ret.company;
-                var siteinfo = resp.ret.website;
-
-                __globals__.baseinfo = {};
-                __globals__.material = {};
-
-                __globals__.baseinfo.type = record.type;;
-                __globals__.baseinfo.serverregion = record.serverregion;
-                __globals__.baseinfo.id = record.id;
-
-                if( company ){
-                    __globals__.companyinfo = {};
-
-                    Object.assign(__globals__.companyinfo,company);
-                }
-                if( siteinfo ){
-                    __globals__.siteinfo = {};
-                    __globals__.domains = [];
-
-                    Object.assign(__globals__.siteinfo,siteinfo);
-                    console.log(siteinfo);
-                    var domains = [];
-                    if( siteinfo.domain1 && siteinfo.domain1.length > 0  ){
-                        domains.push(1);
-                    }
-                    if( siteinfo.domain2 && siteinfo.domain2.length > 0  ){
-                        domains.push(2);
-                    }
-                    if( siteinfo.domain3 && siteinfo.domain3.length > 0  ){
-                        domains.push(3);
-                    }
-                    if( siteinfo.domain4 && siteinfo.domain4.length > 0  ){
-                        domains.push(4);
-                    }
-                    __globals__.domains = domains;
-                    if( siteinfo.hasOwnProperty('accessmethod') ){
-                        __globals__.siteinfo.accessmethod =  JSON.parse(siteinfo.accessmethod);
-                    }
-                    if( siteinfo.hasOwnProperty('ip') ){
-                        //__globals__.siteinfo.ip =  JSON.parse(siteinfo.ip);
-                    }
-                    if( siteinfo.hasOwnProperty('languages') ){
-                        __globals__.siteinfo.languages =  JSON.parse(siteinfo.languages);
-                    }
-                }
-                __globals__.material.sitemanagerurl = record.sitemanagerurl;
-                __globals__.material.checklisturl = record.checklisturl;
-                __globals__.material.protocolurl1 = record.protocolurl1;
-                __globals__.material.protocolurl2 = record.protocolurl2;
-                __globals__.material.securityurl1 = record.securityurl1;
-                __globals__.material.securityurl2 = record.securityurl2;
-
+        this.requestRecord(
+            function(){
                 location.href = "#/reviewrecorddetail";
             },
-            error: function(err){
-                //TODO
+            function(err){
+                console.log("getRecord err")
+                console.log(err);
             }
-        });
-
-        return true;
+        );
     },
     render(){
         let type = this.props.record.type;
