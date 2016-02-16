49275fc35c681c4bc38daa0d846036e6568c02c2
胡家宝 <jiabao_hu@corp.netease.com>
2016-02-16 15:35:15 +0800

    doc0.0.2

--- src/netease/icp/backend/controllers/Controllers.js
+++ src/netease/icp/backend/controllers/Controllers.js
@@ -342,11 +342,11 @@ var StoreService = using('netease.icp.backend.services.StoreService');
          * @apiParam {Number} id 记录ID
          * @apiParam {Number} status 备案申请状态\n0-草稿\n1-初审中\n2-初审未通过\n3-初审已通过\n4-照片审核中\n5-照片审核未通过\n6-照片审核已通过\n7-通管局审核中\n8-通管局审核未通过\n9-通管局审核已通过\n10-未知状态\n
          * @apiParam {String} reasons 通过则为备注,拒绝则为理由(多条用p标签分隔)
-         * @apiParam [{String}] curtainurl 帘布照片URL
-         * @apiParam [{String}] mailingaddress 幕布邮寄地址
-         * @apiParam [{String}] recipient 收件人
-         * @apiParam [{String}] recipientmobile 收件人手机号
-         * @apiParam [{String}] companyname 公司名称
+         * @apiParam {String} [curtainurl] 帘布照片URL
+         * @apiParam {String} [mailingaddress] 幕布邮寄地址
+         * @apiParam {String} [recipient] 收件人
+         * @apiParam {String} [recipientmobile] 收件人手机号
+         * @apiParam {String} [companyname] 公司名称
          *
          * @apiSuccess {Number} ret true:成功,false:失败
          */
