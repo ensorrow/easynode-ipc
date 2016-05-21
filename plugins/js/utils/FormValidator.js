aaee256757a791f410f2cd8a6656f4bb473cf6cf
胡家宝 <jiabao_hu@corp.netease.com>
2016-05-21 22:09:09 +0800

    release;0.0.69:0

--- plugins/js/utils/FormValidator.js
+++ plugins/js/utils/FormValidator.js
@@ -9,31 +9,29 @@ module.exports = {
     regular: function (val, regular) {
         return typeof regular != 'function' ? false : !regular(val);
     },
-    _check: function (o, err, hasError) {
-        var me = this;
-        for(var i in o) {
-            if(o.hasOwnProperty(i)) {
-                if( typeof o[i] == 'object') {
-                    hasError = me._check(o[i], err, hasError);
-                    err = hasError ? err + 1 : err;
-                }else if(o[i] == true && i != 'focus') {
-                    if( o.checked == true ) {
+    check: function (oo) {
+        var hasError = false;
+        var err = 0;
+        function _check (o) {
+            for(var i in o) {
+                if(o.hasOwnProperty(i)) {
+                    if( typeof o[i] == 'object') {
+                        hasError = _check(o[i]);
+                        err = hasError ? err + 1 : err;
+                    }else if(o[i] == true && i != 'focus') {
+                        if( o.checked == true ) {
+                            hasError = false;
+                        } else {
+                            hasError = true;
+                        }
+                        err = hasError ? err + 1 : err;
+                    }else{
                         hasError = false;
-                    } else{
-                        hasError = true;
                     }
-                    err = hasError ? err + 1 : err;
-                }else{
-                    hasError = false;
                 }
             }
         }
-    },
-    check: function (o) {
-        var hasError = false;
-        var err = 0;
-        this._check(o, err, hasError);
-
+        _check(oo);
         return err > 0 ? true : false;
     }
 };
