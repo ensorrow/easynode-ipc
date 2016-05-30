5d384d53b4471128bae90bded3828298a8501f3e
胡家宝 <jiabao_hu@corp.netease.com>
2016-05-30 13:42:15 +0800

    test prof

--- bin/start_dev_servers_node.sh
+++ bin/start_dev_servers_node.sh
@@ -7,7 +7,7 @@ startService() {
         echo "starting  service [$1],  HTTP port: [$2]"
 
         #babel-node --harmony main.js --debug-output=true --http.server.port=$2 --src-dirs=src --main-class=netease.icp.backend.Main --config-files=config/service.conf  --easynode.app.id=$1
-        node  main.min.js --debug-output=true --http.server.port=$2 --src-dirs=src --main-class=netease.icp.backend.Main --config-files=config/service.conf  --easynode.app.id=$1
+        node --prof main.min.js --debug-output=true --http.server.port=$2 --src-dirs=src --main-class=netease.icp.backend.Main --config-files=config/service.conf  --easynode.app.id=$1
 }
 
 echo 'starting icp servers...'
