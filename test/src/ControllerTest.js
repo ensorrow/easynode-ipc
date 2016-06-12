d27c3c6da118700827302b08f524743241d941b0
胡家宝 <jiabao_hu@corp.netease.com>
2016-06-12 22:31:56 +0800

    release:0.0.87:test

--- test/src/ControllerTest.js
+++ test/src/ControllerTest.js
@@ -25,6 +25,7 @@ EasyNode.addArg('easynode-home',process.cwd());
 EasyNode.addSourceDirectory('/node_modules/easynode/lib');
 
 
+
 const logger = using('easynode.framework.Logger').getLogger();
 
 
